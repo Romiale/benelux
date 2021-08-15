075bf6a16fb58cd305adc42b6cb771f128a57f04
ExcaliburSims <ximeonzilu@gmail.com>
2021-08-15 14:59:41 +0100

    perf: Add dependencies

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "36aac52c4690a29a8b2a"; }
+/******/ 		__webpack_require__.h = function() { return "25b522401d02621852e6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
