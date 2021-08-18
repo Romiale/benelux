fdb7af018ad9f1697d2390e321ca1f7a1b39fd74
ExcaliburSims <ximeonzilu@gmail.com>
2021-08-18 13:05:57 +0100

    perf: Add dependencies

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0a97033ec7a149de921c"; }
+/******/ 		__webpack_require__.h = function() { return "e21d9cb23c506995d910"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.10872d1104843a81030e.hot-update.js
+++ .next/static/webpack/webpack.10872d1104843a81030e.hot-update.js
@@ -12,19 +12,8 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "10872d1104843a81030e"; }
+/******/ 	__webpack_require__.h = function() { return "e21d9cb23c506995d910"; }
 /******/ }();
 /******/ 
-/******/ /* webpack/runtime/compat */
-/******/ 
-/******/ 
-/******/ // noop fns to prevent runtime errors during initialization
-/******/ if (typeof self !== "undefined") {
-/******/ 	self.$RefreshReg$ = function () {};
-/******/ 	self.$RefreshSig$ = function () {
-/******/ 		return function (type) {
-/******/ 			return type;
-/******/ 		};
-/******/ 	};
-/******/ }}
+/******/ }
 );
