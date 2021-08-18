580b0c7fc2d315cae5a5d933b3081804c8dff98c
ExcaliburSims <ximeonzilu@gmail.com>
2021-08-18 12:56:53 +0100

    feat: add section4 on apropos pages

--- pages/apropos.js
+++ pages/apropos.js
@@ -1,5 +1,6 @@
 import Layout from "../components/Layout";
 import CardFinalite from "../components/page_apropos/cardFinalite";
+import CardOrganisation from "../components/page_apropos/cardOrganisation";
 import Styles from "../styles/page_apropos/apropos.module.scss";
 
 const Apropos = () => {
@@ -37,16 +38,16 @@ const Apropos = () => {
 					<CardFinalite />
 				</div>
 			</div>
-<<<<<<< HEAD
+
 			<div className={Styles.container_organisation}>
 				<h2 className={Styles.container_title}>Organisation pratique de la Benelux Afro Center</h2>
 				<p>
 					Pour ce faire, BAC Asbl est actif en même temps en Belgique pour une gamme très variée d'activités de coordination du programme financé par la DGD et d'économie sociale, et en République Démocratique du Congo dans les secteurs et domaines d'intervention ci-après :
 				</p>
+				<CardOrganisation />
+				<CardOrganisation />
+				<CardOrganisation/>
 			</div>
-=======
-			<div className={Styles.container_finalite}></div>
->>>>>>> feature-card_organisation
 		</Layout>
 	 );
 }
